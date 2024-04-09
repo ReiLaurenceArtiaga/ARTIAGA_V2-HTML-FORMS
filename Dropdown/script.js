f7790d2c4c378711198c74f4796b1e03c9c17cd6
Rei Laurence Artiaga <rei.artiaga18@gmail.com>
2024-04-09 21:10:20 +0800

    Delete Dropdown directory

--- Dropdown/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdownToggle = document.querySelector(".dropdown-toggle");
-    dropdownToggle.addEventListener("click", () => {
-        const dropdownMenu = document.querySelector("#dynamic-dropdown > .dynamic-dropdown-menu");
-        dropdownMenu.classList.toggle("open");
-        dropdownToggle.classList.toggle("open");
-    });
-});
