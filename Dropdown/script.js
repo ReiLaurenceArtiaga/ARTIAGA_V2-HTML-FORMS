a0d5a75b14a4b8f1aab013da77f148203b5346ae
ReiLaurenceArtiaga <rei.artiaga18@gmail.com>
2024-04-09 14:34:35 +0800

    Dropdown

--- Dropdown/script.js
+++ Dropdown/script.js
@@ -1,8 +1,8 @@
-const dropdownToggle = document.querySelector(".dropdown-toggle");
-
-dropdownToggle.addEventListener("click", () => {
-    const dropdownMenu = document.querySelector("#dropdown > .menu");
-
-    dropdownMenu.classList.toggle("open");
-    dropdownToggle.classList.toggle("open");
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownToggle = document.querySelector(".dropdown-toggle");
+    dropdownToggle.addEventListener("click", () => {
+        const dropdownMenu = document.querySelector("#dynamic-dropdown > .dynamic-dropdown-menu");
+        dropdownMenu.classList.toggle("open");
+        dropdownToggle.classList.toggle("open");
+    });
 });
